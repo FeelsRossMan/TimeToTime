443c6d285528fff04755356e6ace2a87c679eafd
Ross <25727163+FeelsRossMan@users.noreply.github.com>
2022-05-05 21:38:39 -0400

    Managed to implement without the recyclerview method. Not sure why I was doing that in the first place. -Removed TimerAdapter.kt -Removed SettingsFragment.kt (will be brought back in once I have the main functionality of the Timer Fragment in place) -Modified most of the other files in the project

--- app/src/main/java/com/example/timetotime/MainActivity.kt
+++ app/src/main/java/com/example/timetotime/MainActivity.kt
@@ -1,37 +1,30 @@
 package com.example.timetotime
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import com.example.timetotime.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
-        setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        navController = navHostFragment.navController
+        //setupActionBarWithNavController(navController)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -50,9 +43,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        return navController.navigateUp()
+//                || super.onSupportNavigateUp()
+//    }
 }
--- app/src/main/java/com/example/timetotime/TimerFragment.kt
+++ app/src/main/java/com/example/timetotime/TimerFragment.kt
@@ -1,19 +1,20 @@
 package com.example.timetotime
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.timetotime.databinding.FragmentFirstBinding
+import android.view.ViewGroup.LayoutParams
+import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+import com.example.timetotime.databinding.FragmentTimerBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class TimerFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentTimerBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,8 +24,10 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentTimerBinding.inflate(inflater, container, false)
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding!!.newTimerFloatingActionButton.setOnClickListener { newTimerCard() }
+
         return binding.root
 
     }
@@ -37,5 +40,16 @@ class FirstFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun newTimerCard() {
+        val inflater = LayoutInflater.from(context)
+        val layoutParams = LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        layoutParams.bottomMargin = resources.getDimensionPixelOffset(R.dimen.fab_margin)
+        val newTimerView : View = inflater.inflate(R.layout.timer_card, null)
+        newTimerView.layoutParams = layoutParams
+        with(binding.timerListLinearLayoutView) {
+            this.addView(newTimerView,this.childCount-1)
+        }
     }
 }
