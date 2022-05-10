19c1695024bd8975bedbfdac208992dc8569992b
Ross <25727163+FeelsRossMan@users.noreply.github.com>
2022-05-10 07:54:44 -0400

    Moved the TimerDialogFragment.kt code into the TimerFragment.kt class to let me more easily save move data around (probably could've used a view model or something, but this is fine). Got it working so that adding a timer via dialog box sets the TextView on the new timer to the value typed in the dialog box. (Didn't set it up with the interval portion yet)

--- app/src/main/java/com/example/timetotime/TimerFragment.kt
+++ app/src/main/java/com/example/timetotime/TimerFragment.kt
@@ -1,19 +1,28 @@
 package com.example.timetotime
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.timetotime.databinding.FragmentTimerBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
+
+const val TAG = "TimerFragment"
+
 class TimerFragment : Fragment() {
 
+    private var timerInitList: MutableList<Int> = mutableListOf(0)
 
 
     private var _binding: FragmentTimerBinding? = null
@@ -21,6 +30,8 @@ class TimerFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,7 +41,7 @@ class TimerFragment : Fragment() {
 
 
         //TODO: modify once the associated functions are finished
-        _binding!!.newTimerFloatingActionButton.setOnClickListener { newTimerCard() }
+        _binding!!.newTimerFloatingActionButton.setOnClickListener { newTimerDialog() }
         _binding!!.startStopFloatingActionButton.setOnClickListener { startTimer() }
 
         return binding.root
@@ -49,19 +60,57 @@ class TimerFragment : Fragment() {
 
     //TODO: Implement the actual start timer functionality
     private fun startTimer() {
-        val timerDialog = TimerDialogFragment()
-        timerDialog.show(childFragmentManager, TimerDialogFragment.Tag)
+
     }
 
     //TODO: Use the dialog box to create the initial parameters for the timer
-    private fun newTimerCard() {
-        val inflater = LayoutInflater.from(context)
+    private fun newTimerCard(initialTime: Int = 0, initialInterval: Int = 0) {
+
+
+        // Inflate a new timer view using the timer_card.xml layout file
+
         val layoutParams = LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
         layoutParams.bottomMargin = resources.getDimensionPixelOffset(R.dimen.fab_margin)
-        val newTimerView : View = inflater.inflate(R.layout.timer_card, null)
+        val newTimerView : View = layoutInflater.inflate(R.layout.timer_card, null)
         newTimerView.layoutParams = layoutParams
         with(binding.timerListLinearLayoutView) {
             this.addView(newTimerView,this.childCount-1)
+            for (index in 0 until childCount) {
+                val timerCard = this.getChildAt(index)
+                val timerText: TextView = timerCard.findViewById(R.id.running_timer_text)
+
+                //TODO: Setup a function that takes the int value and converts it to a time string
+                timerText.text = timerInitList[index].toString()
+            }
+        }
+    }
+    private fun newTimerDialog() {
+        // Pop up the TimerDialogFragment dialog box to input new timer values
+        val builder = AlertDialog.Builder(this.context)
+        val inflater = layoutInflater
+        val dialogLayout = inflater.inflate(R.layout.new_timer_dialog, null)
+
+        val editText = dialogLayout.findViewById<EditText>(R.id.dialog_timer_time_edit_text)
+
+
+        with(builder) {
+            setPositiveButton("Set", DialogInterface.OnClickListener{ dialog, id ->
+                val textInt = editText.text.toString().toIntOrNull()
+                Log.d(TAG,"${editText.text}")
+                if (textInt == null) {
+
+                    dialog.dismiss()
+                }
+                else {
+                    timerInitList.add(textInt)
+                    newTimerCard(textInt)
+                }
+            })
+            setNegativeButton("Cancel", DialogInterface.OnClickListener{ dialog, id ->
+
+                dialog.dismiss()})
+            setView(dialogLayout)
+            show()
         }
     }
 }
