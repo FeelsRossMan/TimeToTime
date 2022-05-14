6be3452f667479832db68b3a1c0d123285a99295
Ross <25727163+FeelsRossMan@users.noreply.github.com>
2022-05-14 17:57:57 -0400

    adding comments

--- app/src/main/java/com/example/timetotime/TimerViewModel.kt
+++ app/src/main/java/com/example/timetotime/TimerViewModel.kt
@@ -5,24 +5,24 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TimerViewModel: ViewModel() {
+    // holds the init lis for all the timer seconds
     private var _timerTimeInitList: MutableList<Int> = mutableListOf(0)
     val timerTimeInitList: List<Int>
         get() = _timerTimeInitList
 
+    // holds the init list for all the timer intervals
     private var _timerIntervalInitList:  MutableList<Int> = mutableListOf(0)
     val timerIntervalInitList: List<Int>
         get() = _timerIntervalInitList
 
+    // which timer is active?
+    private var activeTimer = 0
 
 
+    // adds the new timer values into the init list. If one of the time values isn't passed it'll just use 0
     fun addNewTimer(newTimeMin: Int? = 0, newTimeSec: Int? = 0, newInterval: Int = 0) {
         val timeInSeconds = (newTimeMin ?: 0) * 60 + (newTimeSec ?: 0)
         _timerTimeInitList.add(timeInSeconds)
         _timerIntervalInitList.add(newInterval)
     }
-
-
-    // Converts from a time int in milliseconds to a time string
-
-
 }
